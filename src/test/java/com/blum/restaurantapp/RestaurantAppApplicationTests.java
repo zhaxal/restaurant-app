1b0113e7d518a753e9871371723f33ff6b0062bc
zhaxa <zhaxa65@gmail.com>
2021-04-22 16:51:30 +0600

    initial commit

---
+++ src/test/java/com/blum/restaurantapp/RestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blum.restaurantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
