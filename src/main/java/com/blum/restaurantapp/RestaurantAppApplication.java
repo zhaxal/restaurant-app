3f8d5213fbb50c639ed3549f25dddac367adaec1
zhaxa <zhaxa65@gmail.com>
2021-04-22 16:54:14 +0600

    initial commit

--- src/main/java/com/blum/restaurantapp/RestaurantAppApplication.java
+++ src/main/java/com/blum/restaurantapp/RestaurantAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestaurantAppApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RestaurantAppApplication.class, args);
+        System.out.println("something");
     }
 
 }
