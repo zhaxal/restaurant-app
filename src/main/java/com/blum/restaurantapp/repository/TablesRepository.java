5c91f42d8964d8ebbec67cdaf9f6d8cbe408acf5
zhaxa <zhaxa65@gmail.com>
2021-04-23 13:58:05 +0600

    fix

--- src/main/java/com/blum/restaurantapp/repository/TablesRepository.java
+++ src/main/java/com/blum/restaurantapp/repository/TablesRepository.java
@@ -1,7 +1,8 @@
 package com.blum.restaurantapp.repository;
 
+import com.blum.restaurantapp.models.Tables;
 import com.blum.restaurantapp.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TablesRepository extends JpaRepository<TablesRepository,Long> {
+public interface TablesRepository extends JpaRepository<Tables,Long> {
 }
